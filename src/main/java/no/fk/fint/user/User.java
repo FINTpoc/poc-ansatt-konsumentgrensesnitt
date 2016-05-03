3d4979d119ed0de24364aa1c18da06f939375eb7
jarlehansen <jarle@jarlehansen.net>
2016-05-03 19:31:47 +0200

    added user endpoint to get orgId

---
+++ src/main/java/no/fk/fint/user/User.java
@@ -0,0 +1,12 @@
+package no.fk.fint.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String orgId;
+}
---
+++ src/main/java/no/fk/fint/user/UserController.java
@@ -0,0 +1,22 @@
+package no.fk.fint.user;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "User")
+@RestController
+public class UserController {
+
+    @ApiOperation("Get user info")
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public User getUser(@RequestHeader(value = "x-org-id", required = false) String orgId) {
+        if (orgId == null)
+            return new User("");
+        else
+            return new User(orgId);
+    }
+}
